33c6e28b48562b4249e61d9a64354928bfcdaad5
loumon koffi samuel <lkoffisamuel@gmail.com>
2022-12-05 15:49:18 +0000

    add scrolling appearance code

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const ratio = .1
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer){
+    entries.forEach(function (entry){
+        if(entry.intersectionRatio > ratio){
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
+    observer.observe(r);
+})
